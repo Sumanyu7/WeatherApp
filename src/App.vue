a00b1d9c3177198b731badc2b3358654059d22ef
sumanyu7 <sumanyu1711@gmail.com>
2021-03-25 20:34:43 +1100

    Current Weather Added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Weather msg="Welcome to Your Vue.js App"/>
+  <h1>Weather App</h1>
+  <Weather/>
 </template>
 
 <script>
 import Weather from './components/Weather.vue'
+
 
 export default {
   name: 'App',
@@ -16,11 +17,15 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 30px;
+  background-color: #b3cdd1;
+  background-image: linear-gradient(315deg, #b3cdd1 0%, #9fa4c4 74%);
+
+  height:100vh;
+
 }
 </style>
--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,58 +1,123 @@
 <template>
-  <div class="hello">
-    <h1 class="text-dark">{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="text-center">
+      <input 
+          class="form-control me-2 search" 
+          type="search" 
+          placeholder="Ex: Sydney" 
+          aria-label="Search" 
+          v-model="city"
+          
+          @keyup.enter="fetchWeather"
+        >
+      <div class="card text-white" v-cloak>
+       <div class="custom-control custom-switch text-right">
+          <input type="checkbox" class="custom-control-input" id="customSwitch1" @click="toggleUnit">
+          <label class="custom-control-label" for="customSwitch1">°F</label>
+        </div>
+        <div class="text-center my-4" v-if="typeof weather.main != 'undefined'" >
+          <h2>{{weather.name}},{{weather.sys.country}}</h2>
+          <h1 class="temp mt-4">{{Math.round(weather.main.temp) }}°</h1>
+          <h6>Feels like {{Math.round(weather.main.feels_like) }}°</h6>
+          <h3 class="my-4 font-italic">{{ weather.weather[0].main }}</h3>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
+
 export default {
   name: 'Weather',
-  props: {
-    msg: String
+  data: () => ({
+    apiKey: 'd4fae1475eed163c09df0c1e56c77ca3',
+    url: 'https://api.openweathermap.org/data/2.5/',
+    city: '',
+    unit: 'metric',
+    weather: {},
+  }),
+  mounted: function () {
+    // On page load
+    let long;
+    let lat;
+    for (var i = 0; i < 2; i++) {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+          // Fetch weather
+          this.locationFound = true;
+          long = position.coords.longitude;
+          lat = position.coords.latitude;
+          fetch(`${this.url}weather/?lat=${lat}&lon=${long}&units=${this.unit}&appid=${this.apiKey}`)
+              .then(res => {
+                return res.json();
+              }).then(res => {
+                this.setWeather(res);
+              });
+          }), (error) => {
+            if (error.code == error.PERMISSION_DENIED) {
+              this.locationFound = false;
+            }
+          };
+      }
+    }
+  },
+  methods: {
+    // for the search bar
+      fetchWeather: async function () {
+        const fullUrl  = `${this.url}weather/?q=${this.city}&units=${this.unit}&appid=${this.apiKey}`;
+            axios
+             .get(fullUrl)
+             .then(response => (this.weather = response.data))   
+      },
+      // for location based weather
+      setWeather(results) {
+      this.weather = results;
+      },
+      //to toggle between C & F
+      toggleUnit: function() {
+         if(this.unit == 'metric'){
+            this.unit = 'imperial'; 
+            alert('Re-enter the city');
+         }else{
+           this.unit = 'metric';
+           alert('Re-enter the city');
+         } 
+         
+      }
   }
+  
 }
+       
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+.search{
+  max-width:28rem;
+  border-radius:30px;
+  margin-top:50px;
+  box-shadow: 0px 10px 10px;
   display: inline-block;
-  margin: 0 10px;
 }
-a {
-  color: #42b983;
+.card{
+  max-width:28rem;
+  margin:30px auto;
+  border-radius:30px;
+  background-color: #7f5a83;
+  background-image: linear-gradient(315deg, #7f5a83 0%, #0d324d 74%);
 }
+
+.temp{
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  font-weight: 700;
+}
+
+
+.custom-switch{
+  margin:20px 20px 0px 0px;
+}
+
 </style>
